83fff6594c23f920b1449e14be5b39776b192454
Marlie Martinez <marlieahue@gmail.com>
2024-07-24 11:47:25 -0400

    finished

---
+++ src/TreeSetExampleTwo.java
@@ -0,0 +1,27 @@
+import java.util.TreeSet;
+
+public class TreeSetExampleTwo {
+    public static void main(String[] args) {
+        TreeSet<Integer> numbers = new TreeSet<>();
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(6);
+        System.out.println("TreeSet: " + numbers);
+
+        // Using the remove() method
+        boolean value1 = numbers.remove(5);
+        System.out.println("Is 5 removed? " + value1);
+
+        // Using the removeAll() method
+        boolean value2 = numbers.removeAll(numbers);
+        System.out.println("Are all elements removed? " + value2);
+
+        // Using the first() method
+        int first = numbers.first();
+        System.out.println("First Number: " + first);
+
+        // Using the last() method
+        int last = numbers.last();
+        System.out.println("Last Number: " + last);
+    }
+}
